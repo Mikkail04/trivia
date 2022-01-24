9ea18f922c988ec87ddf955dbc34506e71685aff
Denny29 <43240668+Denny29@users.noreply.github.com>
2022-01-24 17:29:48 -0500

    Did day 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,23 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+let questNum = 0;
+
 function App() {
-  return <div className="app">Trivia!</div>;
+  return (
+    <div className="app">
+      <Question question={data[questNum].question} />
+      <NextQuestion />
+    </div>
+  );
+}
+
+function Question(props) {
+  return <div>{props.question.text}</div>;
+}
+
+function NextQuestion() {
+  return <button>Next Question</button>;
 }
 
 export default App;
