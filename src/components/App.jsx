5190213088b009b6cb04ea8f09b2cd886fc0d3ad
Denny29 <43240668+Denny29@users.noreply.github.com>
2022-03-08 08:19:10 -0500

    Day 2 goal 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,22 @@ import data from "../sample_data.json";
 function App() {
   console.log(data[0].question.text);
   let questionNum = 0;
+  let answerIndex = data[questionNum].question.correct_choice_index;
+
+  const [answerDisplayed, setDisplay] = useState(false);
   return (
     <div className="app">
       Trivia!
       <Question
         question={data[questionNum].question.text}
-        answers={data[questionNum].question.choices}
+        answers={data[questionNum].question}
+      />
+      <button onClick={() => setDisplay(true)}>
+        Click for the correct answer
+      </button>
+      <ShowAnswer
+        display={answerDisplayed}
+        correct={data[questionNum].question.choices[answerIndex]}
       />
       <NextQuestion />
     </div>
@@ -20,8 +30,8 @@ function Question(props) {
   return (
     <div>
       <p className="question">{props.question}</p>
-      {props.answers.map((answer) => (
-        <Answer answer={answer} />
+      {props.answers.choices.map((answer) => (
+        <Answers answers={answer} />
       ))}
     </div>
   );
@@ -31,8 +41,15 @@ function NextQuestion(props) {
   return <button>Next Question</button>;
 }
 
-function Answer(props) {
-  return <div> {props.answer} </div>;
+function Answers(props) {
+  return <div> {props.answers} </div>;
 }
 
+function ShowAnswer(props) {
+  if (props.display) {
+    return <p> The correct answer is {props.correct} </p>;
+  } else {
+    return <p> The correct answer is unanswered</p>;
+  }
+}
 export default App;
